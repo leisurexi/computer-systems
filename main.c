11c89780fe22ce9af2623242aa7286f1039c7727
leisurexi <leisurexi@gmail.com>
2020-09-13 21:41:30 +0800

    ==> leisurexi

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char* argv[]) {
 //    int x = -1;
@@ -7,12 +8,16 @@ int main(int argc, char* argv[]) {
 //    printf("u=%u=%d\n", u, u);
 //    int result = -2147483648 < 2147483647;
 //    printf("the result: %d", result);
+//
+//    int i = 0;
+//    int arr[3] = {0};
+//    for (; i <= 3; i++) {
+//        arr[i] = 0;
+//        printf("hello world\n");
+//    }
 
-    int i = 0;
-    int arr[3] = {0};
-    for (; i <= 3; i++) {
-        arr[i] = 0;
-        printf("hello world\n");
-    }
+    printf("%u\n", log2(4096));
+
+
     return 0;
 }
